7bc0e6fb6a4dc9af6bdc9f5e15c099e071938b9b
Oliver Ramirez <ramirezoliver.g@gmail.com>
2022-04-06 23:27:26 +0800

    add user tests

--- library/app.py
+++ library/app.py
@@ -1,14 +1,19 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import os
 
 db = SQLAlchemy()
+
+def get_database_url():
+    DATA_BASE_URL = os.environ['DB_URL']
+    return DATA_BASE_URL
 
 def create_app():
     """Construct the core application."""
     app = Flask(__name__)
 
     app.config['SECRET_KEY']='Th1s1ss3cr3t'
-    app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///db.sqlite3'
+    app.config['SQLALCHEMY_DATABASE_URI']=get_database_url()
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
     db.init_app(app)
---
+++ tests/unit_tests/views/test_users.py
@@ -0,0 +1,70 @@
+import pytest
+from library.app import create_app, db
+from library.models.users import Users
+from base64 import b64encode
+
+@pytest.fixture()
+def app():
+    app = create_app()
+    app.config.update({
+        "TESTING": True,
+    })
+
+    # other setup can go here
+
+    yield app
+
+    # clean up / reset resources here
+
+@pytest.fixture()
+def client(app):
+    yield app.test_client()
+
+@pytest.fixture
+def empty_db(app):
+    with app.app_context():
+        db.create_all()
+        yield db
+        db.session.remove()
+        db.drop_all()
+
+USER_1 = dict(
+    id = 1,
+    public_id = 1,
+    name = 'fake_name',
+    password = 'fake_pass',
+    admin = False,
+)
+
+@pytest.fixture
+def database(empty_db):
+    user1 = Users(**USER_1)
+
+    db.session.add_all([user1])
+    db.session.commit()
+
+def tests_register_user(client, database):
+    response = client.post('/register', 
+    json={
+        "name": "test_name",
+        "password": "test_pw",
+        }
+    )
+    assert response.status_code == 200
+
+def tests_login_valid(client, database):
+    client.post('/register', 
+    json={
+        "name": "test_name",
+        "password": "test_pw",
+        }
+    )
+    
+    response = client.get('/login', 
+        headers={"Authorization": "Basic {}".format(b64encode(b"test_name:test_pw").decode("utf8"))}
+    )
+    assert response.status_code==200
+
+def tests_get_authors(client, database):
+    response = client.post('/users')
+    assert response.status_code==200
