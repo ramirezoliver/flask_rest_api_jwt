88cef3e502bde3e2f30ba08de4e6113bab0db2b2
Oliver Ramirez <ramirezoliver.g@gmail.com>
2022-04-20 14:53:50 +0800

    update test_author database

--- tests/functional_tests/views/test_author.py
+++ tests/functional_tests/views/test_author.py
@@ -4,7 +4,7 @@ import pytest
 
 
 @pytest.fixture
-def valid_token_header(client, database):
+def valid_token_header(client, empty_db):
     client.post('/register',
                 json={
                     "name": "test_name",
@@ -19,7 +19,7 @@ def valid_token_header(client, database):
     return headers
 
 
-def tests_create_author(client, database, valid_token_header):
+def tests_create_author(client, empty_db, valid_token_header):
     data = {"name": "J.K. Rowling", "book": "Harry Potter", "country": "U.K."}
     response = client.post('/author',
         headers=valid_token_header,
@@ -27,12 +27,12 @@ def tests_create_author(client, database, valid_token_header):
     assert response.status_code == 201
 
 
-def tests_get_authors(client, database, valid_token_header):
+def tests_get_authors(client, empty_db, valid_token_header):
     response = client.get('/authors', headers=valid_token_header)
     assert response.status_code == 200
 
 
-def tests_delete_existing_author(client, database, valid_token_header):
+def tests_delete_existing_author(client, empty_db, valid_token_header):
     data = {"name": "J.K. Rowling", "book": "Harry Potter", "country": "U.K."}
     response = client.post('/author',
         headers=valid_token_header,
@@ -43,8 +43,8 @@ def tests_delete_existing_author(client, database, valid_token_header):
     assert response.status_code == 202
 
 
-def tests_delete_failed(client, database, valid_token_header):
-    existing_author_id = '99'
-    response = client.delete('/author/' + existing_author_id,
+def tests_delete_failed(client, empty_db, valid_token_header):
+    missing_author_id = '99'
+    response = client.delete('/author/' + missing_author_id,
         headers=valid_token_header)
     assert response.status_code == 404
